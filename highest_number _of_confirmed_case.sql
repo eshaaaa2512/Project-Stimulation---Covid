de818e57f09b2429aa7056d95f126813081388f4
Esha <eshadeswal@gmail.com>
2024-03-18 12:18:37 +0530

    [ESHA] ADD: create a stored procedure for recovered cases

--- highest_number _of_confirmed_case.sql
+++ highest_number _of_confirmed_case.sql
@@ -1,24 +1,49 @@
 CREATE DATABASE COVID_DB;
 USE COVID_DB;
---Which country has the highest number of confirmed cases on a specific date
 
-SELECT 
-    COUNTRY_WISE_LATEST.Country_Region,
-    COUNTRY_WISE_LATEST.Confirmed AS CONFIRMED_CASES,
-    full_grouped.Date
-FROM 
-    COUNTRY_WISE_LATEST
-LEFT OUTER JOIN 
-    full_grouped ON COUNTRY_WISE_LATEST.Country_Region = full_grouped.Country_Region
-WHERE 
-    COUNTRY_WISE_LATEST.Confirmed IN 
-        (SELECT  MAX(COUNTRY_WISE_LATEST.Confirmed) 
-         FROM COUNTRY_WISE_LATEST)
-AND
-    full_grouped.DATE IN
-	  (SELECT MAX(full_grouped.DATE) 
-	  FROM full_grouped);
+
+
+CREATE PROCEDURE GetRecoveredCasesByCountryAndDate
+(
+    @Country VARCHAR(100),
+    @Date DATE)
+AS
+BEGIN
+   
+
+    SELECT 
+        TotalRecovered = covid_19_clean_complete.recovered
+    FROM 
+        covid_19_clean_complete
+	 WHERE 
+        covid_19_clean_complete.Country_Region = @Country  and 
+		covid_19_clean_complete.Date = @date;
+   
+
+    
+END
+GO
+
+
+DROP PROCEDURE GetRecoveredCasesByCountryAndDate
+
+EXECUTE GetRecoveredCasesByCountryAndDate @country ='Afghanistan', @Date='2020-01-22'
+
+select * from covid_19_clean_complete;
+
+
+
+
+
+
+
 	  
 
+      
 
 
+
+
+
+	 
+	 
