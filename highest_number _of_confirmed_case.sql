3f5e9b7a89454e3425f038591d7b27ca6d575539
Esha <eshadeswal@gmail.com>
2024-03-18 10:58:14 +0530

    [ESHA] ADD:  Average new deaths

--- highest_number _of_confirmed_case.sql
+++ highest_number _of_confirmed_case.sql
@@ -1,24 +1,17 @@
 CREATE DATABASE COVID_DB;
 USE COVID_DB;
---Which country has the highest number of confirmed cases on a specific date
 
-SELECT 
-    COUNTRY_WISE_LATEST.Country_Region,
-    COUNTRY_WISE_LATEST.Confirmed AS CONFIRMED_CASES,
-    full_grouped.Date
-FROM 
-    COUNTRY_WISE_LATEST
-LEFT OUTER JOIN 
-    full_grouped ON COUNTRY_WISE_LATEST.Country_Region = full_grouped.Country_Region
-WHERE 
-    COUNTRY_WISE_LATEST.Confirmed IN 
-        (SELECT  MAX(COUNTRY_WISE_LATEST.Confirmed) 
-         FROM COUNTRY_WISE_LATEST)
-AND
-    full_grouped.DATE IN
-	  (SELECT MAX(full_grouped.DATE) 
-	  FROM full_grouped);
+--Calculate the average number of new deaths per day across all countries.
+
+SELECT AVG(worldometer_data.TotalDeaths) as Average_Death from worldometer_data;
+
 	  
 
+      
 
 
+
+
+
+	 
+	 
