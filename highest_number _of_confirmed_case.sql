be252455749e43aafcefc6be512b903fe6df7205
Esha <eshadeswal@gmail.com>
2024-03-18 15:25:17 +0530

    [ESHA] ADD : TSQL

--- highest_number _of_confirmed_case.sql
+++ highest_number _of_confirmed_case.sql
@@ -1,24 +1,32 @@
 CREATE DATABASE COVID_DB;
 USE COVID_DB;
---Which country has the highest number of confirmed cases on a specific date
+
+
+
+--Write a T-SQL query to calculate the total number of cases (confirmed + deaths + recovered) for each country.
 
 SELECT 
-    COUNTRY_WISE_LATEST.Country_Region,
-    COUNTRY_WISE_LATEST.Confirmed AS CONFIRMED_CASES,
-    full_grouped.Date
-FROM 
-    COUNTRY_WISE_LATEST
-LEFT OUTER JOIN 
-    full_grouped ON COUNTRY_WISE_LATEST.Country_Region = full_grouped.Country_Region
-WHERE 
-    COUNTRY_WISE_LATEST.Confirmed IN 
-        (SELECT  MAX(COUNTRY_WISE_LATEST.Confirmed) 
-         FROM COUNTRY_WISE_LATEST)
-AND
-    full_grouped.DATE IN
-	  (SELECT MAX(full_grouped.DATE) 
-	  FROM full_grouped);
+    Country_Region,
+    SUM(Confirmed + Deaths + Recovered) AS TotalNumberOfCases
+FROM
+    covid_19_clean_complete
+GROUP BY
+    Country_Region;
+
+
+
+
+
+
+
+
 	  
 
+      
 
 
+
+
+
+	 
+	 
